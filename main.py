ab33b0456e10294a334a78f5f8796dd7c251f37b
Christian <christianib003@gmail.com>
2023-11-04 21:10:43 +0200

    change print statements to return statements

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ def get_choices():
 def check_win(player_choice, computer_choice):
   print(f"You chose {player_choice}, and Computer chose {computer_choice}.")
   if player_choice == computer_choice:
-    print("It's a tie!")
+    return("It's a tie!")
   
   elif player_choice == "rock":
     if computer_choice == "scissors":
-      print("Rock smashes scissors. You win! :)")
+      return("Rock smashes scissors. You win! :)")
     else:
-      print("Paper covers rock. You lose. :(")
+      return("Paper covers rock. You lose. :(")
 
   elif player_choice == "paper":
     if computer_choice == "rock":
-      print("Paper covers rock. You win! :)")
+      return("Paper covers rock. You win! :)")
     else:
-      print("Scissors cuts paper. You lose. :(")
+      return("Scissors cuts paper. You lose. :(")
 
   elif player_choice == "scissors":
     if computer_choice == "paper":
-      print("Scissors cuts paper. You win! :)")
+      return("Scissors cuts paper. You win! :)")
     else:
-      print("Rock smashes paper. You lose. :(")
+      return("Rock smashes paper. You lose. :(")
 
 choices = get_choices()
 result = check_win(choices["player"], choices["computer"])
+print(result)
